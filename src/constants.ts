f285896103208416f096ddd864d20f0e342f3cb3
Alexey Yarushin <yarushin.dev@gmail.com>
2020-06-05 19:34:14 +0300

    [~] Move secret to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { SignOptions } from 'jsonwebtoken';
 
 export namespace JWT {
-  export const SECRET = process.env.JWT_SECRET;
+  export const SECRET = 'secret';
   export const SIGN_OPTIONS: SignOptions = {
     algorithm: 'RS256',
     expiresIn: '10m',
