77e4451d4a085acfa8cd2311f15c46021e8c0a2a
Alexey Yarushin <yarushin.dev@gmail.com>
2020-07-01 20:22:08 +0300

    Add cache module. Add compression

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ComponentsModule } from './components/components.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    CacheModule.register(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import * as compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.use(compression({ level: 9 }));
+
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
